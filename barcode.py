c4b7f28f804a29d1d809a3a31405ec2696aba2e8
Gaspar Llamazares <45463974+gasparllamazares@users.noreply.github.com>
2020-02-16 20:27:40 +0100

    Add files via upload

---
+++ barcode.py
@@ -0,0 +1,19 @@
+import qrcode
+from cryptography.fernet import Fernet
+from PIL import ImageShow
+file = open('key.key', 'rb')
+key = file.read()
+file.close()
+print(key)
+for n in range(0,1):
+    number = str(n)
+    encoded = number.encode()
+
+    f = Fernet(key)
+    encrypted = f.encrypt(encoded)
+    print(encrypted)
+    img = qrcode.make(encrypted)
+    img.save(f'No{n}.png')
+f2 = Fernet(key)
+decrypted = f2.decrypt(encrypted)
+print(decrypted)
